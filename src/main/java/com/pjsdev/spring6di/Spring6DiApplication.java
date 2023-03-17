3cb93ae4f43568122e4945b1eeb8d6d50e1a260b
pjsdevcom <mail@pjsdev.com>
2023-03-17 08:53:48 +0000

    add context example

--- src/main/java/com/pjsdev/spring6di/Spring6DiApplication.java
+++ src/main/java/com/pjsdev/spring6di/Spring6DiApplication.java
@@ -1,13 +1,21 @@
 package com.pjsdev.spring6di;
 
+import com.pjsdev.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6DiApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+        MyController myController = ctx.getBean(MyController.class);
+
+        System.out.println("In Main method");
+
+        System.out.println(myController.sayHello());
     }
 
 }
---
+++ src/main/java/com/pjsdev/spring6di/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.pjsdev.spring6di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("I'm in the controller!");
+
+        return "Hello!";
+    }
+}
